7b6a84e9ac50581235cff7eaee57b9d517f164ee
Timo Stollenwerk <contact@timostollenwerk.net>
2014-04-08 13:48:39 +0200

    Remove stupid testing.zcml profile.

--- src/plone/app/angularjs/testing.py
+++ src/plone/app/angularjs/testing.py
@@ -1,20 +1,38 @@
-from plone.app.testing import PloneWithPackageLayer
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
 
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
 import plone.app.angularjs
 
 
-PLONE_APP_ANGULARJS = PloneWithPackageLayer(
-    zcml_package=plone.app.angularjs,
-    zcml_filename='testing.zcml',
-    gs_profile_id='plone.app.angularjs:testing',
-    name="PLONE_APP_ANGULARJS")
+class PloneAppAngularJsLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        xmlconfig.file(
+            'configure.zcml',
+            plone.app.angularjs,
+            context=configurationContext
+        )
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'plone.app.angularjs:default')
+
+
+PLONE_APP_ANGULARJS = PloneAppAngularJsLayer()
 
 PLONE_APP_ANGULARJS_INTEGRATION = IntegrationTesting(
-    bases=(PLONE_APP_ANGULARJS, ),
-    name="PLONE_APP_ANGULARJS_INTEGRATION")
+    bases=(PLONE_APP_ANGULARJS,),
+    name="PloneAppAngularJs:Integration"
+)
 
 PLONE_APP_ANGULARJS_FUNCTIONAL = FunctionalTesting(
-    bases=(PLONE_APP_ANGULARJS, ),
-    name="PLONE_APP_ANGULARJS_FUNCTIONAL")
+    bases=(PLONE_APP_ANGULARJS, z2.ZSERVER_FIXTURE),
+    name="PloneAppAngularJs:Functional"
+)
--- src/plone/app/angularjs/tests/test_setup.py
+++ src/plone/app/angularjs/tests/test_setup.py
@@ -9,14 +9,14 @@ from plone.app.angularjs.testing import\
 class TestExample(unittest.TestCase):
 
     layer = PLONE_APP_ANGULARJS_INTEGRATION
-    
+
     def setUp(self):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
         self.qi_tool = getToolByName(self.portal, 'portal_quickinstaller')
-    
+
     def test_product_is_installed(self):
-        """ Validate that our products GS profile has been run and the product 
+        """ Validate that our products GS profile has been run and the product
             installed
         """
         pid = 'plone.app.angularjs'
