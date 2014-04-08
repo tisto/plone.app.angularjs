0648f48ccc9f25756a5b6960bb212907042a53e9
Timo Stollenwerk <contact@timostollenwerk.net>
2014-04-08 13:38:46 +0200

    Fix buildout, add .gitignore.

--- README.rst
+++ README.rst
@@ -1,2 +1,4 @@
+.. contents::
+
 plone.app.angularjs
 ===================
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 version = '1.0'
 
 long_description = (
-    open('README.txt').read()
+    open('README.rst').read()
     + '\n' +
     'Contributors\n'
     '============\n'
@@ -14,38 +14,39 @@ long_description = (
     open('CHANGES.txt').read()
     + '\n')
 
-setup(name='plone.app.angularjs',
-      version=version,
-      description="",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plone.app.angularjs',
+    version=version,
+    description="",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={'test': ['plone.app.testing']},
-      entry_points="""
-      # -*- Entry points: -*-
-  	  [z3c.autoinclude.plugin]
-  	  target = plone
-      """,
-      )
+    ],
+    keywords='',
+    author='',
+    author_email='',
+    url='http://svn.plone.org/svn/collective/',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    extras_require={'test': ['plone.app.testing']},
+    entry_points="""
+    # -*- Entry points: -*-
+	  [z3c.autoinclude.plugin]
+	  target = plone
+    """,
+)
