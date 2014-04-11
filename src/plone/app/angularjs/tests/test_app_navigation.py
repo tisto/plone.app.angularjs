a70b9c44419601bc7f67139a30d5917d85474c35
tisto <tisto@plone.org>
2014-04-11 19:33:06 +0200

    Add PortletNavigation test.

--- src/plone/app/angularjs/tests/test_app_navigation.py
+++ src/plone/app/angularjs/tests/test_app_navigation.py
@@ -12,7 +12,7 @@ from plone.app.angularjs.testing import\
 import json
 
 
-class TestObjectTraversal(unittest.TestCase):
+class TestAngularJsTopNavigation(unittest.TestCase):
 
     layer = PLONE_APP_ANGULARJS_INTEGRATION_TESTING
 
@@ -43,6 +43,52 @@ class TestObjectTraversal(unittest.TestCase):
         view = getMultiAdapter(
             (self.portal, self.request),
             name="angularjs-top-navigation"
+        )
+        view = view.__of__(self.portal)
+
+        self.assertTrue(view())
+        self.assertEqual(
+            json.loads(view()),
+            [{
+                u'id': u'doc1',
+                u'title': u'Document 1',
+                u'description': u'',
+                u'url': '#/doc1',
+            }]
+        )
+
+
+class TestAngularJsPortletNavigation(unittest.TestCase):
+
+    layer = PLONE_APP_ANGULARJS_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.app = self.layer['app']
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_view_is_registered(self):
+        self.assertTrue(
+            getMultiAdapter(
+                (self.portal, self.request),
+                name="angularjs-portlet-navigation"
+            )
+        )
+
+    def test_empty_navigation(self):
+        view = getMultiAdapter(
+            (self.portal, self.request),
+            name="angularjs-portlet-navigation"
+        )
+        view = view.__of__(self.portal)
+        self.assertEqual(json.loads(view()), [])
+
+    def test_document_in_navigation(self):
+        self.portal.invokeFactory('Document', 'doc1', title='Document 1')
+        view = getMultiAdapter(
+            (self.portal, self.request),
+            name="angularjs-portlet-navigation"
         )
         view = view.__of__(self.portal)
 
