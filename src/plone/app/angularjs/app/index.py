ec412ba09d1d49e6c7f3085bca712bc689ea704f
Timo Stollenwerk <contact@timostollenwerk.net>
2014-05-19 18:33:34 +0200

    Add two traversal adapters for site root. Seems like both approaches do not work with the diazo rewriting.

--- src/plone/app/angularjs/app/index.py
+++ src/plone/app/angularjs/app/index.py
@@ -19,11 +19,39 @@ class AngularAppRootView(BrowserView):
             portal.id
         )
 
+
+from zope.component import adapter
+from zope.interface import implementer
+from zope.publisher.interfaces.http import IHTTPRequest
+from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
+from Products.CMFCore.interfaces import IContentish
+from ZPublisher.BaseRequest import DefaultPublishTraverse
+from zope.component import adapts
+from zope.publisher.interfaces import IRequest
+from Products.CMFCore.interfaces._content import ISiteRoot
+
+
+# https://github.com/collective/collective.routes/blob/master/collective/routes/traverser.py
+class AngularAppTraverser(DefaultPublishTraverse):
+    adapts(ISiteRoot, IRequest)
+
     def publishTraverse(self, request, name):
-        """Always return the Angular app when traversing to
-        Plone/index.html/<path>.
-        """
-        # stop traversing
-        request['TraversalRequestNameStack'] = []
-        # always return the angular app root
-        return self.template()
+        return AngularAppRootView(self.context, self.request)()
+        return DefaultPublishTraverse.publishTraverse(self, request, name)
+
+
+# https://github.com/zopefoundation/Zope/blob/master/src/ZPublisher/BaseRequest.py#L320
+@adapter(IPloneSiteRoot, IHTTPRequest)
+@implementer(IPublishTraverse)
+class AngularAppTraverser(object):
+
+    def __init__(self, context, request=None):
+        self.context = context
+        self.request = request
+
+    def publishTraverse(self, request, name):
+        if IContentish.providedBy(self.context) or IPloneSiteRoot.providedBy(self.context):
+            # stop traversing
+            request['TraversalRequestNameStack'] = []
+            # return angular app view
+            return AngularAppRootView(self.context, self.request)()
