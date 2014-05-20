a15eef9ceb1b9863ba74695ade6dcb46c40576a7
Timo Stollenwerk <contact@timostollenwerk.net>
2014-05-20 11:22:38 +0200

    Add some tests for AngularAppRedirectorTraverser.

--- src/plone/app/angularjs/tests/test_traversal.py
+++ src/plone/app/angularjs/tests/test_traversal.py
@@ -7,6 +7,7 @@ from plone.app.testing import TEST_USER_ID
 from plone.app.angularjs.testing import\
     PLONE_APP_ANGULARJS_INTEGRATION_TESTING
 from plone.app.angularjs.traversal import AngularAppPortalRootTraverser
+from plone.app.angularjs.traversal import AngularAppRedirectorTraverser
 
 
 class TestAngularAppPortalRootTraverser(unittest.TestCase):
@@ -29,4 +30,51 @@ class TestAngularAppPortalRootTraverser(unittest.TestCase):
         self.request.URL = 'http://nohost/plone/front-page'
         traversal = AngularAppPortalRootTraverser(self.portal, self.request)
         view = traversal.publishTraverse(self.request, "front-page")
+        self.assertTrue('ng-app' in view)
+
+
+class TestAngularAppRedirectorTraverser(unittest.TestCase):
+
+    layer = PLONE_APP_ANGULARJS_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.app = self.layer['app']
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_document_view_returns_angular_app(self):
+        self.portal.invokeFactory('Document', 'doc1')
+        self.request.URL = 'http://nohost/plone/doc1'
+        traversal = AngularAppRedirectorTraverser(
+            self.portal.doc1, self.request
+        )
+        view = traversal.publishTraverse(self.request, "view")
+        self.assertTrue('ng-app' in view)
+
+    def test_document_edit_returns_angular_app(self):
+        self.portal.invokeFactory('Document', 'doc1')
+        self.request.URL = 'http://nohost/plone/doc1'
+        traversal = AngularAppRedirectorTraverser(
+            self.portal.doc1, self.request
+        )
+        view = traversal.publishTraverse(self.request, "edit")
+        self.assertTrue('ng-app' in view)
+
+    def test_folder_view_returns_angular_app(self):
+        self.portal.invokeFactory('Folder', 'folder1')
+        self.request.URL = 'http://nohost/plone/folder1'
+        traversal = AngularAppRedirectorTraverser(
+            self.portal.folder1, self.request
+        )
+        view = traversal.publishTraverse(self.request, "view")
+        self.assertTrue('ng-app' in view)
+
+    def test_folder_contents_returns_angular_app(self):
+        self.portal.invokeFactory('Folder', 'folder1')
+        self.request.URL = 'http://nohost/plone/folder1'
+        traversal = AngularAppRedirectorTraverser(
+            self.portal.folder1, self.request
+        )
+        view = traversal.publishTraverse(self.request, "folder_contents")
         self.assertTrue('ng-app' in view)
