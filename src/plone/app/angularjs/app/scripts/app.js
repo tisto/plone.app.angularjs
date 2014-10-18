78ee245c4dae2f451f0880510b8787ccd7f0cdcb
Timo Stollenwerk <tisto@plone.org>
2014-10-18 14:58:42 +0200

    Revert "Concat js resources."
    This reverts commit 2e7019e37cfcb916eb0725a1c07b2182ba44074d.

---
+++ src/plone/app/angularjs/app/scripts/app.js
@@ -0,0 +1,44 @@
+var ploneModule = angular.module(
+  'ploneApp',
+  [
+    'ngAnimate',
+    'ngRoute',
+    'ngSanitize',
+    'ui.bootstrap'
+  ]
+);
+
+// Route Configuration
+ploneModule.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    'use strict';
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+    $routeProvider.when('/:objecttraversal*', {
+      controller: 'ObjectPathController',
+      templateUrl: '++theme++plone.app.angularjs/page.tpl.html'
+    }).otherwise({
+      redirectTo: 'front-page'
+    });
+  }
+]);
+
+ploneModule.controller('ObjectPathController',
+  ['$scope', '$routeParams', '$http', '$sce',
+    function($scope, $routeParams, $http, $sce) {
+      'use strict';
+      //if ($routeParams.objecttraversal.match('/edit$')) {
+      //}
+      $http({
+        url: '++api++v1/traversal',
+        method: 'GET',
+        params: {'path': $routeParams.objecttraversal.replace('index.html/', '')},
+      }).success(function(data) {
+        $scope.page = data;
+        $scope.deliberatelyTrustDangerousSnippet = function() {
+          return $sce.trustAsHtml($scope.snippet);
+        };
+      });
+    }
+  ]
+);
---
+++ src/plone/app/angularjs/app/scripts/modal.js
@@ -0,0 +1,46 @@
+var ploneModule;
+
+ploneModule.controller('ModalInstanceCtrl', ['$scope', '$modalInstance', 'items',
+  function ($scope, $modalInstance, items) {
+    'use strict';
+    $scope.items = items;
+    $scope.selected = {
+      item: $scope.items[0]
+    };
+
+    $scope.ok = function () {
+      $modalInstance.close($scope.selected);
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  }
+]);
+
+
+ploneModule.controller('ModalDemoCtrl', ['$scope', '$modal', '$log',
+  function ($scope, $modal, $log) {
+    'use strict';
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.open = function () {
+
+      var modalInstance = $modal.open({
+        templateUrl: '++theme++plone.app.angularjs/edit.tpl.html',
+        controller: 'ModalInstanceCtrl',
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selected) {
+        $scope.selected = selected;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+  }
+]);
