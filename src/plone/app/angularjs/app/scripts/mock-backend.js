01a190c7a7243e20464d7941d505c27317ff8074
Timo Stollenwerk <tisto@plone.org>
2014-10-28 21:54:43 +0000

    Add lorem ipsum text to httpBackend data.

--- src/plone/app/angularjs/app/scripts/mock-backend.js
+++ src/plone/app/angularjs/app/scripts/mock-backend.js
@@ -2,12 +2,13 @@ var ploneModule;
 
 angular.module('e2e-mocks', ['ngMockE2E']).run(function($httpBackend) {
 
+  var loremipsum = '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>';
   var site_structure = [
     {
       id: 'front-page',
       title: 'Welcome to Plone',
       description: 'Congratulations! You have successfully installed Plone.',
-      text: '<p>Front-page content</p>',
+      text: '<h2>Front-page content</h2>' + loremipsum,
       path: 'front-page',
       portal_type: 'document',
       children: []
@@ -16,7 +17,7 @@ angular.module('e2e-mocks', ['ngMockE2E']).run(function($httpBackend) {
       id: 'news',
       title: 'News',
       description: 'Site News',
-      text: '<p>Site news content</p>',
+      text: '<h2>Site news content</h2>' + loremipsum,
       path: 'news',
       portal_type: 'folder',
       children: []
@@ -25,7 +26,7 @@ angular.module('e2e-mocks', ['ngMockE2E']).run(function($httpBackend) {
       id: 'events',
       title: 'Events',
       description: 'Site Events',
-      text: '<p>Site events content</p>',
+      text: '<h2>Site events content</h2>' + loremipsum,
       path: 'events',
       portal_type: 'folder',
       children: []
@@ -34,7 +35,7 @@ angular.module('e2e-mocks', ['ngMockE2E']).run(function($httpBackend) {
       id: 'Members',
       title: 'Members',
       description: 'Site Members',
-      text: '<p>Site Members content</p>',
+      text: '<h2>Site Members content</h2>' + loremipsum,
       path: 'Members',
       portal_type: 'folder',
       children: []
@@ -73,7 +74,7 @@ angular.module('e2e-mocks', ['ngMockE2E']).run(function($httpBackend) {
       'id': value.id,
       'title': value.title,
       'description': value.description,
-      'text': '<p>Lorem Ipsum</p>'
+      'text': value.text
     };
     var traversal_re = new RegExp('\\+\\+api\\+\\+v1/traversal\\?path=' + value.path);
     $httpBackend.whenGET(traversal_re).respond(traversal);
